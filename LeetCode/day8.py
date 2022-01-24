712b05fd01ab29d485e41e86c466663dd0665083
yuanqi <yuanqingxiao@outlook.com>
2022-01-24 14:35:23 -0800

    day8-2

--- LeetCode/day8.py
+++ LeetCode/day8.py
@@ -20,9 +20,68 @@ def twoSum1(nums: list[int], target: int) -> list[int]:
         result[i] = idx
 
 nums = [2,7,11,15]; target = 18
-print(twoSum1(nums,target))
-
-
+# print(twoSum1(nums,target))
 
 # **********************************************
+# 716. Max Stack
+# not solved
+# **********************************************
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+class MaxStack:
+
+    def __init__(self):        
+        self.head = Node('head')
+        self.size = 0
+
+    def push(self, x: int) -> None:
+        node = Node(x)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+
+    def pop(self) -> int:
+        if self.isEmpty():
+            raise Exception('Pop from empty stack')
+        remove = self.head.next
+        self.head.next = self.head.next.next
+        self.size -= 1
+        return remove.value
+
+    def top(self) -> int:
+        if self.isEmpty():
+            raise Exception('Pop from empty stack')
+        remove = self.head.next
+        self.head.next = self.head.next.next
+        # self.size -= 1
+        return remove.value
+
+    def peekMax(self) -> int:
+        if self.isEmpty():
+            raise Exception("Peeking from an empty stack")
+        return self.head.next.value.max()
+
+    def popMax(self) -> int:
+        node = head
+        maxV = node.value
+        while node:
+            if node.value > maxV:
+                maxV = node.value
+            node.next = self.head.next
+
+
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
+
+# **********************************************
+# 56. Merge Intervals
 # **********************************************
