3fa3bbabb1cfcc39dc42f7d8c21535d096e579f5
yuanqi <yuanqingxiao@outlook.com>
2022-01-24 16:40:21 -0800

    day8-2 added

--- LeetCode/day8.py
+++ LeetCode/day8.py
@@ -26,53 +26,50 @@ nums = [2,7,11,15]; target = 18
 # 716. Max Stack
 # not solved
 # **********************************************
-class Node:
-    def __init__(self,value):
-        self.value = value
-        self.next = None
-class MaxStack:
+# class Node:
+#     def __init__(self,value):
+#         self.value = value
+#         self.next = None
+# class MaxStack:
 
-    def __init__(self):        
-        self.head = Node('head')
-        self.size = 0
+#     def __init__(self):        
+#         self.head = Node('head')
+#         self.size = 0
 
-    def push(self, x: int) -> None:
-        node = Node(x)
-        node.next = self.head.next
-        self.head.next = node
-        self.size += 1
+#     def push(self, x: int) -> None:
+#         node = Node(x)
+#         node.next = self.head.next
+#         self.head.next = node
+#         self.size += 1
 
-    def pop(self) -> int:
-        if self.isEmpty():
-            raise Exception('Pop from empty stack')
-        remove = self.head.next
-        self.head.next = self.head.next.next
-        self.size -= 1
-        return remove.value
+#     def pop(self) -> int:
+#         if self.isEmpty():
+#             raise Exception('Pop from empty stack')
+#         remove = self.head.next
+#         self.head.next = self.head.next.next
+#         self.size -= 1
+#         return remove.value
 
-    def top(self) -> int:
-        if self.isEmpty():
-            raise Exception('Pop from empty stack')
-        remove = self.head.next
-        self.head.next = self.head.next.next
-        # self.size -= 1
-        return remove.value
+#     def top(self) -> int:
+#         if self.isEmpty():
+#             raise Exception('Pop from empty stack')
+#         remove = self.head.next
+#         self.head.next = self.head.next.next
+#         # self.size -= 1
+#         return remove.value
 
-    def peekMax(self) -> int:
-        if self.isEmpty():
-            raise Exception("Peeking from an empty stack")
-        return self.head.next.value.max()
+#     def peekMax(self) -> int:
+#         if self.isEmpty():
+#             raise Exception("Peeking from an empty stack")
+#         return self.head.next.value.max()
 
-    def popMax(self) -> int:
-        node = head
-        maxV = node.value
-        while node:
-            if node.value > maxV:
-                maxV = node.value
-            node.next = self.head.next
-
-
-
+#     def popMax(self) -> int:
+#         node = head
+#         maxV = node.value
+#         while node:
+#             if node.value > maxV:
+#                 maxV = node.value
+#             node.next = self.head.next
 
 # Your MaxStack object will be instantiated and called as such:
 # obj = MaxStack()
@@ -84,4 +81,40 @@ class MaxStack:
 
 # **********************************************
 # 56. Merge Intervals
+# append the largest tails only
+# **********************************************
+def merge(intervals: list[list[int]]) -> list[list[int]]:
+    # sort the original list by the first elem
+    intervals.sort(key = lambda x: x[0])
+    
+    # unpack the package intervals as 1,3
+    result = []
+    for x,y in intervals:
+        if not result:
+            result.append([x,y])
+            # for the next loop 
+            continue
+        # comparison, result has had values,get the previous package value
+        # print(result)
+        x0,y0 = result[-1]
+        # merge if prsent is smaller than the previous value y, which is y0
+        
+        if x > y0:
+            # just append, since the previous x0,y0 has been added last time
+            result.append([x,y])
+        else:
+        # we just sorted by the first elem in the first line of the func
+            # the last element is useless
+            # print(result)
+            result.pop()
+            result.append([x0, max(y,y0)])
+    return result
+# intervals = [[1,4],[4,5]]
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+print(merge(intervals))
+
+
+# **********************************************
+# 200. Number of Islands
+# append the largest tails only
 # **********************************************
