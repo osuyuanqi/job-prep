9d118213d02d29f28d73f7f41e4776448a5c7078
yuanqi <yuanqingxiao@outlook.com>
2022-01-26 02:46:18 -0800

    day10 another mtd

--- LeetCode/day10.py
+++ LeetCode/day10.py
@@ -9,33 +9,58 @@ from pub_module import *
 #         self.val = val
 #         self.next = next
 def oddEvenList(self, head: [ListNode]) -> [ListNode]:
-        oddDummy,evenDummy = odd, even = ListNode(0), ListNode(0)
-        isOdd = True
-        while head:
-            if isOdd:
-                odd.next = head
-                odd = odd.next
-            else:
-                even.next = head
-                even = even.next
-            isOdd = not isOdd
-            head = head.next
-        even.next = None
-        odd.next = evenDummy.next
-        return oddDummy.next
+    oddDummy,evenDummy = odd, even = ListNode(0), ListNode(0)
+    isOdd = True
+    while head:
+        if isOdd:
+            odd.next = head
+            odd = odd.next
+        else:
+            even.next = head
+            even = even.next
+        isOdd = not isOdd
+        head = head.next
+    even.next = None
+    odd.next = evenDummy.next
+    return oddDummy.next
 # **********************************************
 # 203. Remove Linked List Elements
 # **********************************************
 
 def removeElements(head: [ListNode], val: int) -> [ListNode]:
-        current = head
-        pre = None
-        while head and head.val == val:
-            head = head.next
-        while current:
-            if current.val == val and pre:
-                pre.next = pre.next.next
-            else:
-                pre = current
-            current = current.next
-        return head
+    current = head
+    pre = None
+    while head and head.val == val:
+        head = head.next
+    while current:
+        if current.val == val and pre:
+            pre.next = pre.next.next
+        else:
+            pre = current
+        current = current.next
+    return head
+
+# **********************************************
+# 141. Linked List Cycle
+# hash/two pointers
+# **********************************************
+
+# method1: hash table
+def hasCycle(head: [ListNode]) -> bool:
+    check = set()
+    while head:
+        if head in check:
+            return True
+        check.add(head)
+        head = head.next
+    return False
+
+# method2: fast/slow ptr 
+def hasCycle1(head: [ListNode]) -> bool:
+    fast, slow = head, head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            return True
+    return False
