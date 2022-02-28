b73ff9f364dfac20a8c7caefa77023980d15885d
yuanqi <yuanqingxiao@outlook.com>
2022-02-28 01:40:01 -0800

    reverse integer added

--- LeetCode/day17.py
+++ LeetCode/day17.py
@@ -24,9 +24,31 @@ def twoSum(numbers: list[int], target: int) -> list[int]:
     else:
         # print(l,h)
         return [l+1, h+1] if l!= h else []
-numbers = [2,7,11,15]
-target = 9
-print(twoSum(numbers,target))
+# numbers = [2,7,11,15]
+# target = 9
+# print(twoSum(numbers,target))
 # **********************************************
 # 15. 3Sum
 # **********************************************
+
+# **********************************************
+# 7. Reverse Integer
+# **********************************************
+def reverse(x:int)->int:
+    sign = 1 if x > 0 else -1
+    rst = sign * int(str(abs(x))[::-1])
+    return 0 if rst > 2 ** 31 - 1 or rst < -2**31 else rst
+def reverse1(x:int)->int:
+    res,sign = 0, 0
+    if x > 0:
+        sign = 1
+    else:
+        sign = -1
+        x = -x
+    while x:
+        res = res * 10 + x % 10
+        x = x // 10
+        # print(x,res)
+    return 0 if res > pow(2,31) else sign*res
+a = -123
+print(reverse1(a))
