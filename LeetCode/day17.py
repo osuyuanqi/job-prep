591b936bd688f67496012c9b9b4191bd65161b2b
yuanqi <yuanqingxiao@outlook.com>
2022-02-27 20:01:09 -0800

    two sumII-double pointer added

--- LeetCode/day17.py
+++ LeetCode/day17.py
@@ -1,7 +1,9 @@
 # **********************************************
 # 167. Two Sum II - Input Array Is Sorted
+# sorted tips
 # **********************************************
 
+# way1: hash
 def twoSum(numbers: list[int], target: int) -> list[int]:
     dic={}
     for i in range(len(numbers)):
@@ -9,10 +11,22 @@ def twoSum(numbers: list[int], target: int) -> list[int]:
             return [dic[target - numbers[i]]+1,i+1]
         dic[numbers[i]] = i
     return []
+
+# way2: double pointer,since it's sorted array
+def twoSum(numbers: list[int], target: int) -> list[int]:
+    l,h = 0, len(numbers) - 1
+    while numbers[l] + numbers[h]!= target and l < h:
+        # print(numbers[l],numbers[h],l,h)
+        if target - numbers[l] > numbers[h]:
+            l+=1
+        else:
+            h-=1
+    else:
+        # print(l,h)
+        return [l+1, h+1] if l!= h else []
 numbers = [2,7,11,15]
 target = 9
 print(twoSum(numbers,target))
-
 # **********************************************
 # 15. 3Sum
 # **********************************************
