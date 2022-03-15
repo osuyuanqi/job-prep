ed1b936ac0eb8a752694f7c5a205f5e3b2ded38e
yuanqi <yuanqingxiao@gmail.com>
2022-03-14 17:52:58 -0700

    day19#38-js addd

--- LeetCode/day19.js
+++ LeetCode/day19.js
@@ -1,8 +1,8 @@
-/**
+/************************************************
  * 53. Maximum Subarray
  * @param {number[]} nums
  * @return {number}
- */
+ ***********************************************/
 var maxSubArray = function(nums) {
     let max = -Infinity;
     let temp = 0;
@@ -10,7 +10,7 @@ var maxSubArray = function(nums) {
         console.log("i=",i,"\tnums=",nums[i],"\ttemp,num+temp=",temp,nums[i]+temp,"\tmax=",Math.max(nums[i],nums[i]+temp))
         // from the temp max to explore until find the maximum one
         temp = Math.max(nums[i],nums[i]+temp)
-            console.log(temp)
+            console.log(temp);
 
         if (temp > max){
             console.log("maxt",temp)
@@ -20,5 +20,57 @@ var maxSubArray = function(nums) {
     return max;
 };
 
-const nums = [-2,1,-3,4,-1,2,1,-5,4];
-console.log(maxSubArray(nums));
+// const nums = [-2,1,-3,4,-1,2,1,-5,4];
+// console.log(maxSubArray(nums));
+
+/************************************************
+ * 38. Count and Say
+ * @param {number} n
+ * @return {string}
+ ***********************************************/
+
+// iterative
+var countAndSay = function(n) {
+    let res = "1";
+    while (n>1){
+    let presentValue=res[0],temp="",count=0;
+    for(let i=0;i<res.length;i++){
+        // calculate the #
+        if(res[i]==presentValue){
+            count++;
+        }
+        // move to the different value
+        else{
+            // store the present state first
+            temp+=count+presentValue;
+            // begin the second calculation
+            count=1;
+            presentValue=res[i];
+        }
+    }
+    // console.log(res,"temp=",temp,"aaa",count,presentValue)
+    // end with first temp string + the final state(count+presentValue)
+    res = temp+count+presentValue;
+    n--;
+    }
+    return res;
+}
+// recursive
+var countAndSay1 = function(n,res = "1") {
+    if(n===1) return res;
+    let presentValue=res[0],temp="",count=0;
+    for(let i=0;i<res.length;i++){
+        if(res[i]==presentValue){
+            count++;
+        }
+        else{
+            temp+=count+presentValue;
+            count=1;
+            presentValue=res[i];
+        }
+    }
+        // console.log(res,count,presentValue)
+    return countAndSay(n-1,temp+count+presentValue);
+}
+
+console.log(countAndSay(4));
