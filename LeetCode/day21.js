8435ec4d63f148ef41de00c7954fbbff03a0f2c9
yuanqi <yuanqingxiao@gmail.com>
2022-03-15 17:45:12 -0700

    day21#70 added

--- LeetCode/day21.js
+++ LeetCode/day21.js
@@ -46,7 +46,46 @@ var generate = function(numRows) {
     return res;
 };
 
-console.log(generate(4));
+// console.log(generate(4));
+/*************************************************
+ * 70. Climbing Stairs
+ * @param {number} n
+ * @return {number}
+ ************************************************/
+
+// iterative->swap
+var climbStairs = function(n) {
+    if (n < 4) return n;
+    let second=2,third=3;
+    for(let i =4;i<n+1;i++){
+        let temp=0;
+        temp=second;
+        second=third;
+        third =second+temp; 
+    }
+    return third;
+    // return climbStairs(n-1)+climbStairs(n-2);
+};
+// iterative->hash array
+var climbStairs = function(n) {
+    let res = [];
+    res[0]=1,res[1]=2;
+    for(let i =2;i<=n;i++)
+        res[i]=res[i-1]+res[i-2];
+    return res[n];
+}
+// recursive->hash dic/array
+// let memo={1:1,2:2,3:3}
+let memo=[0,1,2,3]
+var climbStairs = function(n) {
+    if (memo[n]!=undefined) return memo[n];
+    // res = 
+    memo[n]=climbStairs(n-1)+climbStairs(n-2);
+    return memo[n]
+};
+
+// console.log(climbStairs(45))
+
 
 /*************************************************
  * tips: for..in->attibute,for..of->value
