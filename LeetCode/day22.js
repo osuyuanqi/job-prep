9d5194469494588bbaf36e3ed791b2c14d1c0559
yuanqi <yuanqingxiao@gmail.com>
2022-03-16 16:13:33 -0700

    js-day22#350 added

--- LeetCode/day22.js
+++ LeetCode/day22.js
@@ -60,6 +60,7 @@ var deleteNode = function(node) {
 
 /*************************************************
  350. Intersection of Two Arrays II
+ note the test cases
  ***********************************************/
 
 /**
@@ -67,12 +68,13 @@ var deleteNode = function(node) {
  * @param {number[]} nums2
  * @return {number[]}
  */
+ // dict "of" usage, O(n)
 var intersect = function(nums1, nums2) {
      let arr = [],dict={}
      for(let i of nums1){
-        dict[i]= dict[i]?dict[i]++:1
+        dict[i]= dict[i]?dict[i]+1:1
      }
-     console.log(dict)
+     // console.log(dict)
      for(let j of nums2){
         if (dict[j]){
             dict[j]--;
@@ -81,7 +83,33 @@ var intersect = function(nums1, nums2) {
      }
      return arr;
     }
-let  nums1 =  [4,9,5], nums2 = [9,4,9,8,4]
+
+// rank->push orderly,O(n)
+var intersect = function (nums1,nums2) {
+    // default sort: convert to string and ascend by Unicode value.
+    nums1.sort((a,b)=>{return a-b});
+    nums2.sort((a,b)=>{return a-b});
+    // nums2.sort();
+    // console.log(nums2)
+    // nums2.sort((a,b)=>{return a-b});
+    // console.log(nums2)
+
+    let i=0,j=0,arr=[];
+    while (i<nums1.length && j<nums2.length){
+        if (nums1[i]==nums2[j]){
+            arr.push(nums1[i]);
+            i++;
+            j++;
+        }
+        else if(nums1[i]<nums2[j]) i++;
+        else j++;
+    }
+    return arr;
+}
+// let  nums1 =  [4,9,5], nums2 = [9,4,9,8,4]
+let nums1 = [1,2,2,1], nums2 = [2,2]
+// let nums1=[61,24,20,58,95,53,17,32,45,85,70,20,83,62,35,89,5,95,12,86,58,77,30,64,46,13,5,92,67,40,20,38,31,18,89,85,7,30,67,34,62,35,47,98,3,41,53,26,66,40,54,44,57,46,70,60,4,63,82,42,65,59,17,98,29,72,1,96,82,66,98,6,92,31,43,81,88,60,10,55,66,82,0,79,11,81]
+// nums2=[5,25,4,39,57,49,93,79,7,8,49,89,2,7,73,88,45,15,34,92,84,38,85,34,16,6,99,0,2,36,68,52,73,50,77,44,61,48]
 console.log(intersect(nums1,nums2));
 /*************************************************
  * 155. Min Stack
