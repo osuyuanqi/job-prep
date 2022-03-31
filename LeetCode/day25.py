6e819addcb22bbea693d47c491a9b869571efeb0
yuanqi <yuanqingxiao@gmail.com>
2022-03-30 19:23:09 -0700

    archive day25

--- LeetCode/day25.py
+++ LeetCode/day25.py
@@ -13,9 +13,31 @@ def findRepeatedDnaSequences(s: str) -> list[str]:
     # print(dic,l)
     return set(dic)
 
+# ***********************************************
+# Molecular lab intv: 
+# count the pair of stocks
+# ***********************************************
+        
+def count(n,ar):
+    dic = {}
+    for i in range(len(ar)):
+        if ar[i] in dic:
+            dic[ar[i]]+=1
+        else:
+            dic[ar[i]]=1
+    # print(dic)
+    res=0
+    for i in dic:
+        # print(res,dic[i])
+        res+=dic[i]//2
+    return res
+
 if __name__=="__main__":
     print('tst')
     
     # 187. Repeated DNA Sequences
     s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
     print(findRepeatedDnaSequences(s))
+    # count pair
+    n=9; ar=[10,20,20,10,50,10,10,20,30]
+    print(count(n,ar))
---
+++ LeetCode/day26.py
@@ -0,0 +1,10 @@
+# ***********************************************
+# 187. Repeated DNA Sequences
+# ***********************************************
+
+def plusOne(digits: list[int]) -> list[int]:
+	res = []
+
+	return res
+digits = [1,2,3]
+print(plusOne(digits))
