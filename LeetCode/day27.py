e9f030d95fee8d0d1d2abe80214455045a925936
yuanqi <yuanqingxiao@gmail.com>
2022-04-05 16:33:10 -0700

    day27#122

--- LeetCode/day27.py
+++ LeetCode/day27.py
@@ -11,6 +11,7 @@ def twoSum( nums: list[int], target: int) -> list[int]:
 
 # ***********************************************
 # 121. Best Time to Buy and Sell Stock
+# one transaction
 # ***********************************************
 
 def maxProfit(prices: list[int]) -> int:
@@ -22,6 +23,16 @@ def maxProfit(prices: list[int]) -> int:
             profits = prices[i]- buy
     return profits
 
+# ***********************************************
+# 122. Best Time to Buy and Sell Stock II
+# multiple transaction
+# ***********************************************
+def maxProfit(prices: list[int]) -> int:
+    profits = 0
+    for i in range(0,len(prices)-1):
+        if prices[i]<prices[i+1]:
+            profits+=prices[i+1]-prices[i]
+    return profits
 
 if __name__=="__main__":
     print('tst')
@@ -31,5 +42,10 @@ if __name__=="__main__":
 
     # 121. Best Time to Buy and Sell Stock
     # prices =  [7,6,4,3,1]
-    prices=[7,1,5,3,6,4]
+    # prices=[7,1,5,3,6,4]
+    # print(maxProfit(prices))
+
+    # 122. Best Time to Buy and Sell Stock II
+    # prices = [7,1,5,3,6,4]
+    prices=[1,2,3,4,5]
     print(maxProfit(prices))
