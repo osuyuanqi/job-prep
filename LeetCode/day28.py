c5a7fb671aa96212e5d1643ea82e1e262366f844
yuanqi <yuanqingxiao@gmail.com>
2022-04-07 00:31:40 -0700

    day28#387

--- LeetCode/day28.py
+++ LeetCode/day28.py
@@ -1,9 +1,25 @@
-
 # ***********************************************
 # 387. First Unique Character in a String
 # ***********************************************
 def firstUniqChar(s: str) -> int:
-	return 
+	dic = {}
+	# just access, didn't change
+	for i in range(len(s)):
+		if s[i] not in dic:
+			dic[s[i]] = [1,i]
+		else:
+			dic[s[i]]=2
+	for i in dic.items():
+		k,v=i
+		# means v==[1,i]->2nd val
+		if v!=2:
+			return v[1]
+	return -1
 
 if __name__=="__main__":
 	print('tst')
+	# 387. First Unique Character in a String
+	# s = "leetcode"
+	# print(firstUniqChar(s))
+
+	
