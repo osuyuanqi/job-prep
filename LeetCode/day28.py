02461a5e4a82b0feb69a563fa8f2f33b5c9dba9f
yuanqi <yuanqingxiao@gmail.com>
2022-04-06 22:23:50 -0700

    day28 added

---
+++ LeetCode/day28.py
@@ -0,0 +1,9 @@
+
+# ***********************************************
+# 387. First Unique Character in a String
+# ***********************************************
+def firstUniqChar(s: str) -> int:
+	return 
+
+if __name__=="__main__":
+	print('tst')
