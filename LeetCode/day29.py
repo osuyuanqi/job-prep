7b1ec5fe36e5d2ab59d6d750e993b289f9461e7d
yuanqi <yuanqingxiao@gmail.com>
2022-04-14 02:08:18 -0700

    day29#160

--- LeetCode/day29.py
+++ LeetCode/day29.py
@@ -27,6 +27,34 @@ def mySqrt(x: int) -> int:
 			end = mid-1
 	return p
 
+# **********************************************
+# 160. Intersection of Two Linked Lists
+# hash/arithmetic method
+# **********************************************
+# hash set
+def getIntersectionNode(headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        dic = set()
+        while headA:
+            dic.add(headA)
+            headA=headA.next
+        while headB:
+            if headB in dic:
+                return headB
+            headB=headB.next
+        return None
+#  arithmetic: a+c+(b+same)=b+c+(a+same)
+#  terminate when it's the same
+def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        p1,p2=headA,headB
+        while p1!=p2:
+            p1=headB if p1==None else p1.next
+            p2=headA if p2==None else p2.next
+        return p1
+
 if __name__=="__main__":
 	print('tst')
-	print(mySqrt(76))
+	
+	# 69. Sqrt(x)
+	# print(mySqrt(76))
+	
+	# 160. Intersection of Two Linked Lists
