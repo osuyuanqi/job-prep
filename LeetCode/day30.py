824e72678636c4970f5899b60c9b30027efd42e2
yuanqi <yuanqingxiao@gmail.com>
2022-04-18 00:32:30 -0700

    day30#189

---
+++ LeetCode/day30.py
@@ -0,0 +1,28 @@
+# ***********************************************
+# 189. Rotate Array
+# ***********************************************
+
+# faster->nums[:] change original
+def rotate(nums: list[int], k: int) -> None:
+	"""
+	Do not return anything, modify nums in-place instead.
+	"""
+	n=len(nums)
+	k %=n
+	nums[:] = nums[n-k:]+nums[:n-k]
+
+# pop original array->change,append into the tail	
+def rotate(nums: list[int], k: int) -> None:
+	n=len(nums)
+	k %=n
+	for i in range(len(nums)-k):
+		x=nums.pop(0)
+		nums.append(x)
+
+if __name__=="__main__":
+	print('tst')
+	
+	# remove the last k elems,prepend to the begin
+	nums = [1,2,3,4,5,6]; k =11
+	print(rotate(nums,k))
+	print(nums)
