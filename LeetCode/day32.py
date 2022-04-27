d8ec890e6f7fee2fa4911af26dbc0ebcdfa9206b
yuanqi <yuanqingxiao@gmail.com>
2022-04-26 17:13:18 -0700

    day32#13

--- LeetCode/day32.py
+++ LeetCode/day32.py
@@ -117,7 +117,35 @@ def deleteNode(node):
 	"""
 	node.val=node.next.val
 	node.next= node.next.next
-	
+
+# **********************************************
+# 13. Roman to Integer
+# **********************************************
+def romanToInt(s: str) -> int:
+	dic ={"I":1,"V":5,"X":10,"L":50,"C":100
+,"D":500,"M":1000}
+	prev,res=s[0],dic[s[0]]
+	for i in range(1,len(s)):
+		# print(dic[prev],dic[s[i]],i)
+		if dic[prev]>dic[s[i]] or dic[prev]==dic[s[i]]:
+			res+=dic[s[i]]
+		else:
+			# before calculation, the smaller value has calculatd once and become the previous one's next
+			res+=dic[s[i]]-2*dic[prev]
+		prev=s[i]
+	return res
+# brilliant way: only concern about +/- current branch
+def romanToInt(s: str) -> int:
+	dic ={"I":1,"V":5,"X":10,"L":50,"C":100
+,"D":500,"M":1000}
+	res=0
+	for i in range(len(s)-1):
+		if dic[s[i+1]]>dic[s[i]]:
+			res-=dic[s[i]]
+		else:
+			res+=dic[s[i]]
+	return res+dic[s[len(s)-1]]
+
 if __name__=="__main__":
 	print('tst')
 	
@@ -132,3 +160,7 @@ if __name__=="__main__":
 	# 169. Majority Element
 	# nums = [3,2,1,2,4,2]
 	# print(majorityElement(nums))
+
+	# 13. Roman to Integer
+	# s = "III"
+	# print(romanToInt(s))	
