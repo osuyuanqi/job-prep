3d076fde0f7f014d8ffee2691e7b680de0e14c98
yuanqi <yuanqingxiao@gmail.com>
2022-05-15 04:22:33 -0700

    day36#160

--- LeetCode/day36.js
+++ LeetCode/day36.js
@@ -62,3 +62,44 @@ var strStr=function (haystack,needle) {
 
 // let haystack = "hello", needle = "ll"
 // console.log(strStr(haystack,needle))
+
+
+// 160. Intersection of Two Linked Lists
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+// math:a+b+c=a+c+b
+var getIntersectionNode = function(headA, headB) {
+    let p1=headA,p2=headB;
+    while (p1!=p2){
+        // headA and headB stay at the original position
+        p1=p1==null?headB:p1.next;
+        p2=p2==null?headA:p2.next;
+    }
+    return p2
+};
+
+// hash set
+var getIntersectionNode = function(headA, headB) {
+    const set=new Set()
+    while(headA){
+        set.add(headA)
+        headA=headA.next
+    }
+    while(headB){
+        if (set.has(headB)){
+            return headB
+        }
+        headB=headB.next
+    }
+};
