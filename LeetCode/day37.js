b16a8152bbfa7afc6ef0b4246249e290b9d57dd6
yuanqi <yuanqingxiao@gmail.com>
2022-05-21 05:56:02 -0700

    day37#125

--- LeetCode/day37.js
+++ LeetCode/day37.js
@@ -2,12 +2,24 @@
  * @param {number} n - a positive integer
  * @return {number} - a positive integer
  */
- var reverseBits = function(n) {
+var reverseBits = function (n) {
   // console.log()
-     let b =n.toString(2)
-     // console.log(b)
-     let a=b.padStart(32,'0')
-     // console.log(a);supplement 0
-     let rev=a.split('').reverse().join('')
-     return parseInt(rev,2)
- };
+  let b = n.toString(2);
+  // console.log(b)
+  let a = b.padStart(32, "0");
+  // console.log(a);supplement 0
+  let rev = a.split("").reverse().join("");
+  return parseInt(rev, 2);
+};
+
+// 125. Valid Palindrome
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  const a = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  return a.split("").reverse().join("") === a;
+};
+let s = "A man, a plan, a canal: Panama";
+console.log(isPalindrome(s));
