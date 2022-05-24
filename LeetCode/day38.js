da521fadd6a05612a38593e8a3c3d62da3efbcd6
yuanqi <yuanqingxiao@gmail.com>
2022-05-24 16:13:11 -0700

    day38#2108

--- LeetCode/day38.js
+++ LeetCode/day38.js
@@ -28,8 +28,8 @@ var validPalindrome = function (s) {
 };
 // way2: move left and right explaination
 function isPar(s) {
-    // console.log(s)
-    return s===s.split("").reverse().join("")
+  // console.log(s)
+  return s === s.split("").reverse().join("");
 }
 var validPalindrome = function (s) {
   let l = 0,
@@ -38,14 +38,68 @@ var validPalindrome = function (s) {
   // res = 0;
   while (l < r) {
     if (s[l] !== s[r]) {
-        // move right, skip l->[l+1,r+1) ;or move left, skip r->[l,r)
-        // console.log(s[l],s[r])
-      return isPar(s.substring(l+1,r+1))||isPar(s.substring(l,r));
+      // move right, skip l->[l+1,r+1) ;or move left, skip r->[l,r)
+      // console.log(s[l],s[r])
+      return isPar(s.substring(l + 1, r + 1)) || isPar(s.substring(l, r));
     }
     l++;
     r--;
   }
   return true;
 };
-const s = "aeab";
-console.log(validPalindrome(s));
+// const s = "aeab";
+// console.log(validPalindrome(s));
+
+// 2108. Find First Palindromic String in the Array
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function (words) {
+  let res = "";
+  //   res = words.forEach((word) => {
+  //       console.log(word);
+  //     if (word === word.split("").reverse().join(""))
+  //     {return word;}
+
+  //   });
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === words[i].split("").reverse().join("")) {
+      res = words[i];
+      return res;
+    }
+  }
+  return res;
+};
+// filter
+var firstPalindrome = function (words) {
+  let res = "";
+  res = words.filter((word) => {
+    if (word === word.split("").reverse().join("")) {
+      return word;
+    }
+  });
+  console.log(res);
+  return res.length > 0 ? res[0] : "";
+};
+// performance improvemant: cal half
+var firstPalindrome = function (words) {
+  let res = "";
+  for (const word of words) {
+    let isP = true;
+    for (let i = 0; i < Math.floor(word.length / 2); i++) {
+      if (word[i] !== word[word.length - i - 1]) {
+        // console.log(word[i],word[word.length-i-1])
+        isP = false;
+        break;
+      }
+    }
+    if (isP) {
+      res = word;
+      break;
+    }
+  }
+  return res;
+};
+// const words = ["ded", "ghi"];
+// console.log(firstPalindrome(words));
