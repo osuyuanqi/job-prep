d23b0daf61d4583d2b8c4ba2d0fe10be64ca71c8
yuanqi <yuanqingxiao@gmail.com>
2022-06-02 00:51:41 -0700

    day39#104 iterative method

--- LeetCode/day39.js
+++ LeetCode/day39.js
@@ -50,44 +50,65 @@ console.log(fizzBuzz(3));
 var singleNumber = function (nums) {
   const obj = {};
   for (let i = 0; i < nums.length; i++) {
-      if (nums[i] in obj){
-        obj[nums[i]]=0;
-      }
-      else{
-        obj[nums[i]]=1
-      }
+    if (nums[i] in obj) {
+      obj[nums[i]] = 0;
+    } else {
+      obj[nums[i]] = 1;
+    }
   }
-  for(const i in obj){
-      if (obj[i]==1){
-          return i
-      }
+  for (const i in obj) {
+    if (obj[i] == 1) {
+      return i;
+    }
   }
 };
 // bitwise operation
-var singleNumber = function(nums) {
-    return nums.reduce((acc,pre)=>acc^=pre,0)
- };
+var singleNumber = function (nums) {
+  return nums.reduce((acc, pre) => (acc ^= pre), 0);
+};
 
-const nums = [4,1,2, 2, 1];
+const nums = [4, 1, 2, 2, 1];
 console.log(singleNumber(nums));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 104. Maximum Depth of Binary Tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (root == null) {
+    return 0;
+  }
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+// iterative
+var maxDepth = function (root) {
+  // when the root ==[]
+  if (!root) return 0;
+  let stack = [root],
+    depth = 0;
+  while (stack.length) {
+    let new_stack = [];
+    for (const i of stack) {
+      if (i.left) new_stack.push(i.left);
+      if (i.right) new_stack.push(i.right);
+    }
+    // console.log(new_stack)
+    // same logic as pop the root elem
+    stack = new_stack;
+    depth++;
+  }
+  return depth;
+};
+console.log(Math.max(2, 3));
 
 //345. Reverse Vowels of a String
 /**
