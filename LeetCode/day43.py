02f1248bc1cedb1c9a423d47e6def89dad0e4e0f
yuanqi <yuanqingxiao@gmail.com>
2022-06-22 15:26:35 -0700

    day43#13,169,217,242,49

--- LeetCode/day43.py
+++ LeetCode/day43.py
@@ -8,9 +8,88 @@ def romanToInt(s: str) -> int:
         else:
             res+=dic[s[i]]-2*dic[s[i-1]]
     return res
+# 169. Majority Element
+def majorityElement(nums):
+    dic={}
+    for i in nums:
+        if i not in dic:
+            dic[i]=1
+        else:
+            dic[i]+=1
+    maj,res=1,nums[0]
+    print(dic)
+    for k,v in dic.items():
+        if v>maj:
+            maj=v
+            res=k
+            # print(k,v)
+    return res
 
+# 217. Contains Duplicate
+def containsDuplicate(nums)->bool:
+    return len(set(nums))!=len(nums)
+def containsDuplicate(nums)->bool:
+    _set=set()
+    for i in nums:
+        if i in _set:
+            return True
+        _set.add(i)
+    return False
+
+# 242. Valid Anagram
+def isAnagram(s: str, t: str) -> bool:
+    if len(s)!=len(t): return False
+    dic={}
+    for i in s:
+        if i not in dic:
+            dic[i]=1
+        else:
+            dic[i]+=1
+    # print(dic)
+    for j in t:
+        # print(j)
+        if j not in dic:
+            return False
+        else:
+            dic[j]-=1
+            if dic[j]<0:
+                return False
+    return True
+def isAnagram(s: str, t: str) -> bool:
+    return sorted(s)==sorted(t)
+    
+# 49. Group Anagrams
+from collections import defaultdict 
+def groupAnagrams(strs: list[str]) -> list[list[str]]:
+    dic={}
+    for word in strs:
+        # sorted anagram should be consistent
+        sortedWord=''.join(sorted(word))
+        if sortedWord not in dic:
+            dic[sortedWord] = [word]
+        else:
+            dic[sortedWord].append(word)
+    # res = defaultdict(list)
+    return dic.values()
 if __name__=="__main__":
-    print('test')
+    print('success')
+
     # 13
     # s = "MCMXCIV"
     # print(romanToInt(s))
+
+    # 169
+    # nums = [2,2,1,1,1,2,2]
+    # print(majorityElement(nums))
+    
+    # 217
+    # nums = [1,2,3,1]
+    # print(containsDuplicate(nums))
+
+    # 242
+    # s = "anagram"; t = "nagaram"
+    # print(isAnagram(s,t))
+
+    # 49
+    # strs = ["eat","tea","tan","ate","nat","bat"]
+    # print(groupAnagrams(strs))
