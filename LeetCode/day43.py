cb7363975cc9e4ccc06544fae46b3f77abf954a8
yuanqi <yuanqingxiao@gmail.com>
2022-06-23 10:20:55 -0700

    amazon oa

--- LeetCode/day43.py
+++ LeetCode/day43.py
@@ -71,6 +71,43 @@ def groupAnagrams(strs: list[str]) -> list[list[str]]:
             dic[sortedWord].append(word)
     # res = defaultdict(list)
     return dic.values()
+
+# Amazon OA
+def deliveryPlan(allLocations, numDeliveries):
+    # Write your code here
+    res=[];dic={}
+    for i in allLocations:
+        distance = pow(i[0]*i[0]+i[1]*i[1],0.5)
+        dic[distance]=[[i[0],i[1]]]
+    a=[[k,v] for k,v in sorted(dic.items(),key=lambda item:item[0])]
+    # print(a)
+    for i in range(numDeliveries):
+        res.append(a[i][1])
+
+    return res
+
+def applicationPairs(deviceCapacity, foregroundAppList, backgroundAppList):
+    dic={}
+    for i in foregroundAppList:
+        dic[i[0]]=i[1]
+    a=[[k,v] for k,v in sorted(dic.items(),key=lambda item:item[0],reverse=True)]
+    # print(a)
+    res=[]
+    for i in backgroundAppList:
+        for j in a:
+            temp=i[1]+j[1]
+            if temp<=deviceCapacity:
+                res.append([temp,[i[0],j[0]]])
+                break
+    maxV=res[0][0];maxR=0
+    
+    final =[]
+    for i in res:
+        if i[0]>=maxV:
+            maxV=i[0]
+            maxR=i[1]
+    return maxR
+
 if __name__=="__main__":
     print('success')
 
@@ -93,3 +130,18 @@ if __name__=="__main__":
     # 49
     # strs = ["eat","tea","tan","ate","nat","bat"]
     # print(groupAnagrams(strs))
+
+    # Amazon OA
+    # allLocations=[[1,2],[3,4],[1,-1]]
+    # print(deliveryPlan(allLocations,2))
+
+    # deviceCapacity=10
+    # foregroundAppList=[[1,3],[2,5],[3,7],[4,10]]
+    # backgroundAppList=[[1,2],[2,3],[3,4],[4,5]]
+
+    # print(applicationPairs(deviceCapacity,foregroundAppList,backgroundAppList))
+
+    # store foregroundAppList ,O(n)
+    # ranking O(n)
+    # check the max capacity, O(n^2)
+    # output max index, O(n)
