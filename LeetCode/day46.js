5e13ed55b43eb75ddf190ab941f26df40c0528b5
yuanqi <yuanqingxiao@gmail.com>
2022-07-10 13:46:21 -0700

    day46#160

--- LeetCode/day46.js
+++ LeetCode/day46.js
@@ -13,21 +13,30 @@
  * @return {ListNode}
  */
 // infinite loop if no intersect
- var getIntersectionNode = function(headA, headB) {
-    let l1=headA,l2=headB;
-    while (l1!==l2){
-        // transition to the headB, not l2, since l2 is changing.
-        l1=l1?l1.next:headB;
-        l2=l2?l2.next:headA;
-    }
-    return l1;
+var getIntersectionNode = function (headA, headB) {
+  let l1 = headA,
+    l2 = headB;
+  while (l1 !== l2) {
+    // transition to the headB, not l2, since l2 is changing.
+    l1 = l1 ? l1.next : headB;
+    l2 = l2 ? l2.next : headA;
+  }
+  return l1;
 };
 
-// 
-var getIntersectionNode = function(headA, headB) {
-    const h1=new Set()
-    for (let i of headA){
-        h1.add(i)
-        i=i.next;
+// hash map method
+ var getIntersectionNode = function(headA, headB) {
+    const h1 = new Set();
+  while (headA) {
+    h1.add(headA);
+    headA=headA.next;
+  }
+    // console.log(h1);
+  while (headB) {
+    if (h1.has(headB)) {
+        return headB;
     }
-}
+    headB=headB.next;
+  }
+  return null;
+};
