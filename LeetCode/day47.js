d64ad6a921cb2d76e690e6dc9a1789db12cce637
yuanqi <yuanqingxiao@gmail.com>
2022-07-19 14:45:58 -0700

    day47 added

---
+++ LeetCode/day47.js
@@ -0,0 +1,11 @@
+// 28. Implement strStr()
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    
+};
+const haystack = "hello", needle = "ll";
+console.log(strStr());
