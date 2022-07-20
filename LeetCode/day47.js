c31ffbb7f1ea2d4cb109c9d36eaa90cd49f24e6d
yuanqi <yuanqingxiao@gmail.com>
2022-07-20 16:56:22 -0700

    day47#28

--- LeetCode/day47.js
+++ LeetCode/day47.js
@@ -4,8 +4,18 @@
  * @param {string} needle
  * @return {number}
  */
- var strStr = function(haystack, needle) {
-    
+var strStr = function (haystack, needle) {
+  let maxLen = haystack.length - needle.length + 1;
+  if (maxLen > 0) {
+    for (let i = 0; i < maxLen; i++) {
+      if (haystack.slice(i, i + needle.length) === needle) {
+        //   console.log(i);
+        return i;
+      }
+    }
+  }
+  return -1;
 };
-const haystack = "hello", needle = "ll";
-console.log(strStr());
+const haystack = "aaaaa",
+  needle = "aaaaa";
+console.log(strStr(haystack, needle));
